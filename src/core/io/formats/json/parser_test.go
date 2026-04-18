328e72402c20d7f6595782424fd18d1b09fcf429
Romain Mularczyk <romain.mularczyk@gmail.com>
2026-04-18 23:24:05 +0200

    refactor: add test cases with placeholders and empty JSON

--- src/core/io/formats/json/parser_test.go
+++ src/core/io/formats/json/parser_test.go
@@ -1,21 +1,93 @@
 package json
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
+	"trains/src/core/parser"
+	"trains/src/core/translation"
 )
 
-func TestParseSimpleJson(t *testing.T) {
-	reader := JSONReader{}
-	var result map[string]any
-
-	err := reader.Read("../../../../../tests/simple.json", &result)
-	if err != nil {
-		t.Errorf("Error reading simple.json: %v", err)
+func TestParseSimpleNestedKeysJson(t *testing.T) {
+	input := map[string]any{
+		"user": map[string]any{
+			"name": "Jovan",
+		},
 	}
-	parser := JSONParser{}
-	translationSet := parser.Parse(result)
+	jsonParser := JSONParser{}
+	result := jsonParser.Parse(input)
+	expected := []translation.TranslationUnit{
+		{
+			Fullkey:  "user.name",
+			Path:     []string{"user", "name"},
+			Source:   "Jovan",
+			Target:   "",
+			Segments: []parser.Segment{{parser.TextSegment, "Jovan"}},
+		},
+	}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected translation set to be %v, got %v", expected, result)
+	}
+}
 
-	if len(translationSet.Units) != 6 {
-		t.Errorf("Expected 6 units, got %d", len(translationSet.Units))
+func TestParseSimpleFlatJson(t *testing.T) {
+	input := map[string]any{
+		"user": "Jovan",
+	}
+	jsonParser := JSONParser{}
+	result := jsonParser.Parse(input)
+	expected := []translation.TranslationUnit{
+		{
+			Fullkey:  "user",
+			Path:     []string{"user"},
+			Source:   "Jovan",
+			Target:   "",
+			Segments: []parser.Segment{{parser.TextSegment, "Jovan"}},
+		},
+	}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected translation set to be %v, got %v", expected, result)
+	}
+}
+
+func TestParseSimpleNestedJsonWithPlaceholder(t *testing.T) {
+	input := map[string]any{
+		"user": map[string]any{
+			"name": "Jovan",
+		},
+		"grade": map[string]any{
+			"description": "You achieved a grade of {{grade}}",
+		},
+	}
+	jsonParser := JSONParser{}
+	result := jsonParser.Parse(input)
+	expected := []translation.TranslationUnit{
+		{
+			Fullkey:  "user.name",
+			Path:     []string{"user", "name"},
+			Source:   "Jovan",
+			Target:   "",
+			Segments: []parser.Segment{{parser.TextSegment, "Jovan"}},
+		},
+		{
+			Fullkey:  "grade.description",
+			Path:     []string{"grade", "description"},
+			Source:   "You achieved a grade of {{grade}}",
+			Target:   "",
+			Segments: []parser.Segment{{parser.TextSegment, "You achieved a grade of "}, {parser.PlaceholderSegment, "grade"}},
+		},
+	}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected translation set to be %v, got %v", expected, result)
+	}
+}
+
+func TestParseEmptyJson(t *testing.T) {
+	input := map[string]any{}
+	jsonParser := JSONParser{}
+	result := jsonParser.Parse(input)
+	expected := []translation.TranslationUnit{}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected translation set to be %v, got %v", expected, result)
 	}
 }
