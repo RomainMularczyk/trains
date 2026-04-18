cf2b9e19c3cc7a77ee94a0068b63e349dc82517e
Romain Mularczyk <romain.mularczyk@gmail.com>
2026-04-18 23:20:29 +0200

    refactor: replace explicit individual checks with reflect DeepEqual checks

--- src/core/parser/segment_test.go
+++ src/core/parser/segment_test.go
@@ -8,8 +8,8 @@ import (
 func TestSegmentizeOnlyText(t *testing.T) {
 	input := "This is a test"
 	result := Segmentize(input)
-	expected := Segment{TextSegment, "This is a test"}
-	if !reflect.DeepEqual(result[0], expected) {
+	expected := []Segment{{TextSegment, "This is a test"}}
+	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Expected segment to be %v, got %v", expected, result)
 	}
 }
@@ -26,66 +26,44 @@ func TestSegmentizeTextAndPlaceholder(t *testing.T) {
 func TestSegmentizeTextPlaceholderAndText(t *testing.T) {
 	input := "This is a {{placeholder}} and this is another text"
 	result := Segmentize(input)
-	if len(result) != 3 {
-		t.Errorf("Expected 3 segments, got %d", len(result))
-	}
-	if result[0].Type != TextSegment {
-		t.Errorf("Expected segment type to be TextSegment, got %d", result[0].Type)
-	}
-	if result[1].Type != PlaceholderSegment {
-		t.Errorf("Expected segment type to be PlaceholderSegment, got %d", result[1].Type)
-	}
-	if result[2].Type != TextSegment {
-		t.Errorf("Expected segment type to be TextSegment, got %d", result[2].Type)
+	expected := []Segment{{TextSegment, "This is a "}, {PlaceholderSegment, "placeholder"}, {TextSegment, " and this is another text"}}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected segment to be %v, got %v", expected, result)
 	}
 }
 
 func TestSegmentizePlaceholderAndTextAndPlaceholder(t *testing.T) {
 	input := "{{placeholder}} and this is another text {{another_placeholder}}"
 	result := Segmentize(input)
-	if len(result) != 3 {
-		t.Errorf("Expected 3 segments, got %d", len(result))
-	}
-	if result[0].Type != PlaceholderSegment {
-		t.Errorf("Expected segment type to be PlaceholderSegment, got %d", result[0].Type)
-	}
-	if result[1].Type != TextSegment {
-		t.Errorf("Expected segment type to be TextSegment, got %d", result[1].Type)
-	}
-	if result[2].Type != PlaceholderSegment {
-		t.Errorf("Expected segment type to be PlaceholderSegment, got %d", result[2].Type)
+	expected := []Segment{{PlaceholderSegment, "placeholder"}, {TextSegment, " and this is another text "}, {PlaceholderSegment, "another_placeholder"}}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected segment to be %v, got %v", expected, result)
 	}
 }
 
 func TestSegmentizeOnlyOnePlaceholder(t *testing.T) {
 	input := "{{placeholder}}"
 	result := Segmentize(input)
-	if len(result) != 1 {
-		t.Errorf("Expected 1 segment, got %d", len(result))
-	}
-	if result[0].Type != PlaceholderSegment {
-		t.Errorf("Expected segment type to be PlaceholderSegment, got %d", result[0].Type)
+	expected := []Segment{{PlaceholderSegment, "placeholder"}}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected segment to be %v, got %v", expected, result)
 	}
 }
 
 func TestSegmentizeOnlyTwoAdacentPlaceholders(t *testing.T) {
 	input := "{{placeholder}}{{another_placeholder}}"
 	result := Segmentize(input)
-	if len(result) != 2 {
-		t.Errorf("Expected 2 segments, got %d", len(result))
-	}
-	if result[0].Type != PlaceholderSegment {
-		t.Errorf("Expected segment type to be PlaceholderSegment, got %d", result[0].Type)
-	}
-	if result[1].Type != PlaceholderSegment {
-		t.Errorf("Expected segment type to be PlaceholderSegment, got %d", result[1].Type)
+	expected := []Segment{{PlaceholderSegment, "placeholder"}, {PlaceholderSegment, "another_placeholder"}}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected segment to be %v, got %v", expected, result)
 	}
 }
 
 func TestSegmentizeEmptyString(t *testing.T) {
 	input := ""
 	result := Segmentize(input)
-	if len(result) != 0 {
-		t.Errorf("Expected 0 segments, got %d", len(result))
+	expected := []Segment{}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected segment to be %v, got %v", expected, result)
 	}
 }
