d31e3746a0783c485fc1a3261b4871895848beb6
Romain Mularczyk <romain.mularczyk@gmail.com>
2026-04-19 03:06:35 +0200

    refactor: JSONParser to work with fileContent input channel and TranslationUnit output channel

--- src/core/io/formats/json/parser.go
+++ src/core/io/formats/json/parser.go
@@ -9,28 +9,30 @@ import (
 
 type JSONParser struct{}
 
-func (p *JSONParser) Parse(data any) []translation.TranslationUnit {
-	var translationSet []translation.TranslationUnit
-	p.walk(data, []string{}, &translationSet)
-	return translationSet
+func (p *JSONParser) Parse(
+	data <-chan any,
+	translationUnit chan<- translation.TranslationUnit,
+) {
+	fileContent := <-data
+	p.walk(fileContent, []string{}, translationUnit)
 }
 
 func (p *JSONParser) walk(
 	node any,
 	path []string,
-	translationSet *[]translation.TranslationUnit,
+	translationUnit chan<- translation.TranslationUnit,
 ) {
 	switch node.(type) {
 	// object case
 	case map[string]any:
 		for key, value := range node.(map[string]any) {
-			p.walk(value, append(path, key), translationSet)
+			p.walk(value, append(path, key), translationUnit)
 		}
 	// array case
 	case []any:
 		for index, value := range node.([]any) {
 			i := strconv.Itoa(index)
-			p.walk(value, append(path, i), translationSet)
+			p.walk(value, append(path, i), translationUnit)
 		}
 	// when a leaf node is reached (i.e. a string)
 	case string:
@@ -42,6 +44,6 @@ func (p *JSONParser) walk(
 			Source:   node.(string),
 			Segments: parser.Segmentize(node.(string)),
 		}
-		*translationSet = append(*translationSet, unit)
+		translationUnit <- unit
 	}
 }
