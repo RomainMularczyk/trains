05961143ef226dd59032e12d2f912c3c8a9c793a
Romain Mularczyk <romain.mularczyk@gmail.com>
2026-04-19 03:08:40 +0200

    refactor: FileExplorer to output filePaths channel

--- src/core/io/file_explorer.go
+++ src/core/io/file_explorer.go
@@ -5,22 +5,14 @@ import (
 	"path/filepath"
 )
 
-func FileExplorerWorker(root string, reader FileReader, filePool chan<- string) error {
-	defer close(filePool)
-
-	// handle case in which we don't have a base directory
-	// defined
-	if root == "" {
-		root = "."
-	}
-
+func FileExplorerWorker(root string, reader FileReader, filePaths chan<- string) error {
 	return filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
 		if !d.IsDir() && reader.Supports(path) {
-			filePool <- path
+			filePaths <- path
 		}
 
 		return nil
