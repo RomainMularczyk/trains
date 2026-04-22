f4dea6893caa37dd560177eb16fec39bb69b26cb
Romain Mularczyk <romain.mularczyk@gmail.com>
2026-04-23 00:24:41 +0200

    refactor: raise custom TrainsError when input file format is unsupported

--- src/core/config/types/file_format.go
+++ src/core/config/types/file_format.go
@@ -1,6 +1,9 @@
 package configTypes
 
-import "fmt"
+import (
+	"fmt"
+	"trains/src/core/errors"
+)
 
 type FileFormat string
 
@@ -9,13 +12,20 @@ const (
 	YAML FileFormat = "yaml"
 )
 
-func FlagToFileFormat(flag string) (FileFormat, error) {
+/*
+Converts a flag to a file format.
+*/
+func FlagToFileFormat(flag string) (FileFormat, *errors.TrainsError) {
 	switch flag {
 	case "json":
 		return JSON, nil
 	case "yaml":
 		return YAML, nil
 	default:
-		return JSON, fmt.Errorf("Unsupported format: %s", flag)
+		return JSON, &errors.TrainsError{
+			Code:    errors.InvalidConfigError,
+			Message: "Invalid file format",
+			Err:     fmt.Errorf("Invalid file format: %s", flag),
+		}
 	}
 }
