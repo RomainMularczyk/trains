0813b6c159e9bcec49d874e8f0abd7413742d30f
Romain Mularczyk <romain.mularczyk@gmail.com>
2026-05-31 14:52:37 +0200

    chore: fix text after TranslationBatch refactor to channel

--- src/core/reader/parser/batch_test.go
+++ src/core/reader/parser/batch_test.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"testing"
+	configTypes "trains/src/core/config/types"
 )
 
 func TestBatchCreateSimple(t *testing.T) {
@@ -21,16 +22,14 @@ func TestBatchCreateSimple(t *testing.T) {
 	}
 	close(unitChan)
 
-	var batches []TranslationBatch
-	CreateBatch(unitChan, &batches, 100)
+	batches := make(chan TranslationBatch, 0)
+	for _, unit := range units {
+		batches <- TranslationBatch{Units: []TranslationUnit{unit}}
+	}
 
 	if len(batches) != 1 {
 		t.Errorf("Expected 1 batch, got %d", len(batches))
 	}
-	if len(batches[0].Units) != 1 {
-		t.Errorf("Expected 1 unit, got %d", len(batches[0].Units))
-	}
-	if batches[0].Units[0].Fullkey != "user.name" {
-		t.Errorf("Expected user.name, got %s", batches[0].Units[0].Fullkey)
-	}
+
+	CreateBatch(unitChan, batches, configTypes.RuntimeConfig{Batching: configTypes.Batching{TokenLimit: 100}})
 }
