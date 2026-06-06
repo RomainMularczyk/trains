1118c5a8f4245ebe5d1c5b30fbda84dd3bcf6bb6
Romain Mularczyk <romain.mularczyk@gmail.com>
2026-06-06 14:49:17 +0200

    chore: add utility to open JSON file with no error handling

--- src/utils/file.go
+++ src/utils/file.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"encoding/json"
+	"os"
+)
+
 /*
 Check if the file is empty.
 */
@@ -8,4 +13,15 @@ func IsFileEmpty(fileContent []byte) bool {
 		return true
 	}
 	return false
+}
+
+/*
+Opens a JSON file and unmarshals it into a map.
+*/
+func OpenJsonFile(path string) any {
+	inputFile, _ := os.ReadFile(path)
+	var input any
+	json.Unmarshal(inputFile, &input)
+
+	return input
 }
